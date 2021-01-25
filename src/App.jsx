4b6b387398ec127155b118fea78b021785535e85
Maksim1509 <maaa23@mail.ru>
2021-01-25 22:17:48 +0300

    Fixed route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import Login from './pages/Login';
 import Main from './pages/Main';
+
+const renderPrivate = (isAuth, children) => ({ location }) => (
+  isAuth ? children : (<Redirect to={{ pathname: '/', state: { from: location } }} />)
+);
+
+const PrivateRoute = (props) => {
+  const { isAuth } = useSelector((state) => state.auth);
+  const { children } = props;
+  return (
+    <Route
+      render={renderPrivate(isAuth, children)}
+    />
+  );
+};
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
       <Switch>
         <Route path='/' exact component={Login} />
-        <Route path='/main' component={Main} />
+        <PrivateRoute>
+          <Main/>
+        </PrivateRoute>
       </Switch>
     </div> 
   </BrowserRouter>
